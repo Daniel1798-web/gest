d5b8b66c19e179b5fed86576dffead2eb2129a3f
Daniel1798-web <danrieloto@gmail.com>
2022-03-06 19:19:46 -0300

    añadimos router en app

--- src/router.js
+++ src/router.js
@@ -1,16 +1,22 @@
-import {createWebHistoy, createRouter} from 'vue-router'
+import {createWebHistory, createRouter} from 'vue-router'
 
 import home from '@/views/home.vue'
+import stock from '@/views/stock.vue'
 
-const history = createWebHistoy();
+const history = createWebHistory();
 
 export default createRouter({
     history,
     routes: [
         {
-            path:"/",
+            path:"/home",
             name:"home",
             component:home
+        },
+        {
+            path:"/stock",
+            name:"stock",
+            component:stock
         }
     ]
 
--- src/views/stock.vue
+++ src/views/stock.vue
@@ -1,5 +1,12 @@
 <template>
-    {{hi}}
+ <header class="cabecera" >
+    <button>Back</button>
+    <input type="text"  class="texto" placeholder="Añadir Producto" v-model="value"/>
+    <input type="text"  class="texto" placeholder="Añadir fecha" v-model="value2"/>
+    <button  class="añadirL" v-on:click="añadir()">Añadir a Lacteos</button>
+    <button  class="añadirC" v-on:click="añadir2()">Añadir a Critico</button>
+    <button  class="borrar" v-on:click="eliminar()" >Borrar</button>
+</header>
 </template>
 
 <script>
